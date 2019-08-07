4bac647e458788ff506e5456473f3a6f57b2b615
Rehan Mallick <rmallick@ellation.com>
2019-08-06 21:47:18 -0700

    updates

--- src/templates/FilmProject/filmProject.jsx
+++ src/templates/FilmProject/filmProject.jsx
@@ -3,19 +3,25 @@ import Helmet from "react-helmet";
 import Layout from "../../layout";
 import config from "../../../data/SiteConfig";
 
+import './filmProject.scss';
+
 export default class FilmProjectTemplate extends React.Component {
   render() {
     const { pageContext } = this.props;
     const { id, bannerImg, header, awards, paragraphs } = pageContext;
     return (
       <Layout>
-        <div>
-          <Helmet>
-            <title>{`${header || config.siteTitle}`}</title>
-          </Helmet>
-          <img src={bannerImg}></img>
-          <div>
-						<h1>{header}</h1>
+        <Helmet><title>{`${header || config.siteTitle}`}</title></Helmet>
+        <div className='film-project main-banner-container container'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <div 
+                className='banner-img-container'
+                style={{backgroundImage: `url(${bannerImg})`}} />
+            </div>
+            <div className='col-md-6 banner-header-container'>
+              <div className='movie-headline'>{header}</div>
+            </div>
           </div>
         </div>
       </Layout>
